7c0a1f1f3e48fbb0b6b3934a743f337bfce4b013
tugyad <doloresz.tugya@stud.uni-corvinus.hu>
2022-11-19 17:31:27 +0100

    feladat 6

--- gyak04adatbazis/Form1.cs
+++ gyak04adatbazis/Form1.cs
@@ -14,15 +14,47 @@ namespace gyak04adatbazis
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+        Microsoft.Office.Interop.Excel.Application xlApp; 
+        Microsoft.Office.Interop.Excel.Workbook xlWB; 
+        Microsoft.Office.Interop.Excel.Worksheet xlSheet;
 
         void LoadData()
         {
             Flats = context.Flats.ToList();
         }
+
+        void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+               
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
 
         }
     }
