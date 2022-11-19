26d369e7d94dc6f751be5f3ed9bc8af6db2103e1
tugyad <doloresz.tugya@stud.uni-corvinus.hu>
2022-11-19 18:02:16 +0100

    7.7

--- gy04v2/gy04v2/Form1.cs
+++ gy04v2/gy04v2/Form1.cs
@@ -92,9 +92,25 @@ namespace gy04v2
                 counter++;
             }
         }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
 
 
-     		
-         
-}
+
+
+    }
 }
