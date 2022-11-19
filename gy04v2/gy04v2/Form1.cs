47be3fdaa3abf854f6a2292424ec8858e49bf8c8
tugyad <doloresz.tugya@stud.uni-corvinus.hu>
2022-11-19 18:02:56 +0100

    7.8

--- gy04v2/gy04v2/Form1.cs
+++ gy04v2/gy04v2/Form1.cs
@@ -91,6 +91,9 @@ namespace gy04v2
 
                 counter++;
             }
+            xlSheet.get_Range(GetCell(2, 1), GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            
         }
         private string GetCell(int x, int y)
         {
