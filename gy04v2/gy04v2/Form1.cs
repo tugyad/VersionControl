6b551074d894f1ef0413ad3589da6840176d7d36
tugyad <doloresz.tugya@stud.uni-corvinus.hu>
2022-11-19 17:55:59 +0100

    7.3 javított

--- gy04v2/gy04v2/Form1.cs
+++ gy04v2/gy04v2/Form1.cs
@@ -7,13 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 namespace gy04v2
 {
     public partial class Form1 : Form
     {
         RealEstateEntities context = new RealEstateEntities();
-        List<Flat> Flats;
+        public List<Flat> Flats;
+        Excel.Application xlApp; 
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,7 +27,55 @@ namespace gy04v2
         }
         private void LoadData()
         {
-            Flats = context.Flats.ToList();
+            Flats = context.Flat.ToList();
+            
         }
-    }
+        void CreateExcel()
+        {
+
+        
+        try
+        {
+         xlApp = new Excel.Application();
+         xlWB = xlApp.Workbooks.Add(Missing.Value);
+         xlSheet = xlWB.ActiveSheet;
+
+         CreateTable();
+
+         xlApp.Visible = true;
+         xlApp.UserControl = true;
+        }
+        catch (Exception ex) 
+        {
+        string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+         MessageBox.Show(errMsg, "Error");
+
+        xlWB.Close(false, Type.Missing, Type.Missing);
+         xlApp.Quit();
+         xlWB = null;
+         xlApp = null;
+        }
+        }
+        void CreateTable()
+        {
+            string[] headers = new string[] {
+         "Kód",
+         "Eladó",
+         "Oldal",
+         "Kerület",
+        "Lift",
+        "Szobák száma",
+        "Alapterület (m2)",
+        "Ár (mFt)",
+        "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+        }
+
+     		
+         
+}
 }
