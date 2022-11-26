542dc89bd7842bcb5f51996d22268e274f695ad3
Dolóresz Tugya <doloresz.tugya@bcsconsult.hu>
2022-11-26 08:54:07 +0100

    kövi órai commit

--- negyedikgyakorlat/Properties/Settings.Designer.cs
+++ negyedikgyakorlat/Properties/Settings.Designer.cs
@@ -8,21 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace negyedikgyakorlat.Properties
-{
+namespace negyedikgyakorlat.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=bit.uni-corvinus.hu;Initial Catalog=RealEstate;Persist Security Info=" +
+            "True;User ID=hallgato;Password=Password123")]
+        public string RealEstateConnectionString {
+            get {
+                return ((string)(this["RealEstateConnectionString"]));
             }
         }
     }
