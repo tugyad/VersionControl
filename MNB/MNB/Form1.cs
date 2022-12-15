b0a837f48410fcab5cc39864722f5988dfa5964f
tugyad <doloresz.tugya@stud.uni-corvinus.hu>
2022-12-15 19:05:50 +0100

    chart

--- MNB/MNB/Form1.cs
+++ MNB/MNB/Form1.cs
@@ -10,6 +10,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 
@@ -47,6 +48,22 @@ namespace MNB
             return result;
 
         }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
+            ReadXml();
+            
+            chartRateData.DataSource = Rates;
+            chartRateData.Series[0].ChartType = SeriesChartType.Line;
+            chartRateData.Series[0].XValueMember = "date";
+            chartRateData.Series[0].YValueMembers = "value";
+            chartRateData.Series[0].BorderWidth = 2;
+            chartRateData.Legends[0].Enabled = false;
+            chartRateData.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chartRateData.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            chartRateData.ChartAreas[0].AxisY.IsStartedFromZero = false;
+        }
         private void ReadXml()
         {
             XmlDocument xml = new XmlDocument();
@@ -72,6 +89,8 @@ namespace MNB
                     }
                 }
             }
+
+
         }
 
 
