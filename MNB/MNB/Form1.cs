ec8bc16123c6b38707d14571f2dbdf927282019a
tugyad <doloresz.tugya@stud.uni-corvinus.hu>
2022-12-15 19:12:45 +0100

    final

--- MNB/MNB/Form1.cs
+++ MNB/MNB/Form1.cs
@@ -20,13 +20,16 @@ namespace MNB
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<string> Currencies = new BindingList<string>();
         public Form1()
         {
             
             InitializeComponent();
             dataGridView1.DataSource = Rates;
-            GetRates();
+           // GetRates();
+            GetCurrencies();
             RefreshData();
+            
 
         }
 
@@ -48,7 +51,24 @@ namespace MNB
             return result;
 
         }
-
+        void GetCurrencies()
+        {
+            MNBArfolyamServiceSoapClient m = new MNBArfolyamServiceSoapClient();
+            GetCurrenciesRequestBody request = new GetCurrenciesRequestBody();
+            GetCurrenciesResponseBody response = m.GetCurrencies(request);
+            string result = response.GetCurrenciesResult;
+            XmlDocument x = new XmlDocument();
+            x.LoadXml(result);
+            MessageBox.Show(result);
+            XmlElement item = x.DocumentElement;
+            int i = 0;
+            while (item.ChildNodes[0].ChildNodes[i] != null)
+            {
+                Currencies.Add(item.ChildNodes[0].ChildNodes[i].InnerText);
+                i++;
+            }
+            m.Close();
+        }
         private void RefreshData()
         {
             Rates.Clear();
