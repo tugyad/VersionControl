58e1fa8139921aaead0406d77bf3b50d1a512433
tugyad <doloresz.tugya@stud.uni-corvinus.hu>
2022-12-17 13:44:04 +0100

    4.fel

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -12,11 +12,14 @@ namespace VaR
 {
     public partial class Form1 : Form
     {
-        
+        List<Tick> Ticks;
+        PortfolioEntities context = new PortfolioEntities();
 
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Tick.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
